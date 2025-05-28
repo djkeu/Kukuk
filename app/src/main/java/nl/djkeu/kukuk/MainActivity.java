c4291a26da111dad670b1c86342b2876ebe961d0
Marc Kooij <mrkooij@gmail.com>
2025-05-28 22:20:02 +0200

    MainActivity: webView.loadUrl

--- app/src/main/java/nl/djkeu/kukuk/MainActivity.java
+++ app/src/main/java/nl/djkeu/kukuk/MainActivity.java
@@ -1,4 +1,19 @@
 package nl.djkeu.kukuk;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/kukuk.html");
+    }
 }
