9039933f45489ad9822ae917991d7050e9a77830
Marc Kooij <mrkooij@gmail.com>
2025-05-29 11:33:56 +0200

    Fix JavaScript not working problem

--- app/src/main/java/nl/djkeu/kukuk/MainActivity.java
+++ app/src/main/java/nl/djkeu/kukuk/MainActivity.java
@@ -1,6 +1,9 @@
 package nl.djkeu.kukuk;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,8 +15,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView webView = findViewById(R.id.webview);
-        webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
+        webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage cm) {
+                Log.d("WEBVIEW_JS", cm.message() + " at " + cm.sourceId() + ":" + cm.lineNumber());
+                return true;
+            }
+        });
+
+        webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("file:///android_asset/kukuk.html");
     }
 }
