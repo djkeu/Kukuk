71c5772e6d7f1ee1b76891ebbfcb55359b4e6ce8
Marc Kooij <mrkooij@gmail.com>
2025-06-02 14:26:17 +0200

    Create imageManager.js

---
+++ app/src/main/assets/js/imageManager.js
@@ -0,0 +1,31 @@
+export class ImageManager {
+    constructor(imageContainer, imagePaths) {
+        this.container = imageContainer;
+        this.images = imagePaths;
+        this.alternateState = false;
+        this.intervalId = null;
+    }
+
+    startAlternating() {
+        this.intervalId = setInterval(() => this.toggle(), 1000);
+    }
+
+    stopAlternating() {
+        if (this.intervalId) {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+        }
+    }
+
+    toggle() {
+        const kukuImage = this.container.querySelector('.kuku-image');
+        this.alternateState = !this.alternateState;
+        kukuImage.src = this.alternateState ? this.images.left : this.images.right;
+        kukuImage.style.display = 'block';
+    }
+
+    hideImage() {
+        const kukuImage = this.container.querySelector('.kuku-image');
+        kukuImage.style.display = 'none';
+    }
+}
